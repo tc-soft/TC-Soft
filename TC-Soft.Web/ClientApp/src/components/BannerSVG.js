73cace6c54640b2b3742acf235597e7b7bc19d34
Tadeusz Ciszewski <office@tc-soft.pl>
2020-09-03 22:54:35 +0200

    feat: Add animation to BannerSVG

--- TC-Soft.Web/ClientApp/src/components/BannerSVG.js
+++ TC-Soft.Web/ClientApp/src/components/BannerSVG.js
@@ -9,44 +9,58 @@ function BannerSVG() {
                     version="1.1" viewBox="0 0 162 100">
                     <g>
                         <g>
+                            {/* Chmura */}
                             <g>
                                 <ellipse className="fill11" cx="118" cy="18" rx="12" ry="11" />
                                 <ellipse className="fill11" cx="143" cy="20" rx="10" ry="9" />
                                 <ellipse className="fill11" cx="130" cy="13" rx="14" ry="13" />
-                                <rect className="fill11" x="117" y="24" width="25.1908" height="4.68506" />
+                                <rect className="fill11" x="117" y="24.3" width="25.1908" height="4.68506" />
                             </g>
                             <polygon className="fill10 animArrowDown" points="127,17 122,24 116,17 120,17 120,10 123,10 123,17 " />
                             <polygon className="fill12 animArrowUp" points="142,17 137,10 132,17 135,17 135,24 139,24 139,17 " />
                         </g>
-                        <rect className="fill00" x="26" y="49" width="20.493" height="20.3005" />
+                        
+                        {/* Drzwi budynku */}
+                        <rect className="fill12" x="26" y="49" width="20.493" height="20.3005" />
                         <polygon className="fill11" points="4,38 37,22 69,38 69,69 49,69 49,47 24,47 24,69 4,69 " />
+                        
                         <polygon className="fill00" points="20,70 53,70 48,75 26,75 " />
                         <rect className="fill10" x="31" y="34" width="11.5431" height="7.83314" />
-                        <line className="line11" x1="31" y1="38" x2="43" y2="38" />
                         
-                        <rect className="fill06" x="126" y="29" width="6.02735" height="58.37" />
+                        {/* Okno budynku */}
+                        <line className="fillNone str5" x1="31" y1="38" x2="42.5" y2="38" />
                         
-                        <line className="line11" x1="37" y1="42" x2="37" y2="34" />
+                        {/* Magistrala chmurowa */}
+                        <rect className="fill06" x="126" y="28.5" width="6.02735" height="58.5" />
+                        
+                        {/* Linia w oknie */}
+                        <line className="fillNone str5" x1="37" y1="42" x2="37" y2="34" />
+                        
+                        {/* Magistrala chmurowa - linia czarna */}
+                        <line className="line00-1" x1="129" y1="87" x2="129" y2="29" />
                         
                         <g>
-                            <rect className="fill00" transform="matrix(2.80341E-14 -1.05851 1.51238 4.00547E-14 126.99 53.1949)" width="5.02279" height="2.81276" />
-                            <rect className="fill00" transform="matrix(2.80341E-14 -1.05851 1.51238 4.00547E-14 126.99 47.8774)" width="5.02279" height="2.81276" />
+                            <rect className="fill00 animModem2Rcv" transform="matrix(2.80341E-14 -1.05851 1.51238 4.00547E-14 126.99 53.1949)" width="5.02279" height="2.81276" />
+                            <rect className="fill00 animModem2Snd" transform="matrix(2.80341E-14 -1.05851 1.51238 4.00547E-14 126.99 47.8774)" width="5.02279" height="2.81276" />
                         </g>
-                        <polygon className="fill00" points="14,17 19,17 19,25 14,28 " />
+
+                        <polygon className="fill00" points="14,17 19,17 19,26 14,28 " />
                         <polygon className="fill12" points="0,35 37,16 73,35 73,38 37,19 0,38 " />
-                        <line className="line00" x1="129" y1="87" x2="129" y2="29" />
                         
                         <rect className="fill11" x="106" y="67" width="46.2097" height="9.03616" rx="2" ry="2" />
                         <rect className="fill00" x="146" y="73" width="4.24123" height="1.18755" />
+                        
+                        {/* Środkowa magistrala */}
                         <rect className="fill06" x="85" y="45" width="6.02735" height="55.2507" />
-                        <rect className="fill06" transform="matrix(2.07513E-14 -1 0.522681 1.76675E-14 84.6231 50.8919)" width="6.02735" height="79.3601" />
-                        <line className="line00-1" x1="88" y1="100" x2="88" y2="48" />
-                        <line className="line00-1" x1="129" y1="48" x2="88" y2="48" />
-                        <line className="line00-02" x1="88" y1="94" x2="7" y2="94" />
+                        <rect className="fill06" x="85" y="45" width="41" height="6.02735" />
+                        
+                        <line className="line00-1" x1="88" y1="100" x2="88" y2="47.5" />
+                        <line className="line00-1" x1="127" y1="48" x2="88" y2="48" />
+                        <line className="line00-05" x1="88" y1="94" x2="7" y2="94" />
                         <g>
-                            <rect className="fill00" transform="matrix(1.75239E-13 -2.02368 1.32333 1.07193E-14 36.2786 95.0047)" width="9.64375" height="0.602735" />
-                            <rect className="fill00" x="31" y="92" width="5.31666" height="4.25397" />
-                            <rect className="fill00" x="37" y="92" width="5.31666" height="4.25397" />
+                            <rect className="fill00" transform="matrix(1.75239E-13 -2.02368 1.32333 1.07193E-14 36.2786 95.0047)" width="9.9" height="0.5" />
+                            <rect className="fill00 animModem1Snd" x="31.5" y="92" width="5.31666" height="4.25397" />
+                            <rect className="fill00 animModem1Rcv" x="36.5" y="92" width="5.31666" height="4.25397" />
                         </g>
                         <rect className="fillNone str4" x="109" y="69" width="9.44284" height="1.8082" />
                         <rect className="fillNone str4" x="119" y="69" width="9.44284" height="1.8082" />
