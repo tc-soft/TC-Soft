732f5d28d16156bb0e5c7abfb744addb5ad74548
Tadeusz Ciszewski <office@tc-soft.pl>
2020-12-06 19:09:28 +0100

    feat: Add react-ga library for use Google Analytics

--- TC-Soft.Web/ClientApp/src/App.js
+++ TC-Soft.Web/ClientApp/src/App.js
@@ -1,40 +1,33 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-
+import { Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Implementations from './pages/Implementations';
 import Gdpr from './pages/Gdpr';
 import Cookies from './pages/Cookies';
+import usePageTracking from './components/usePageTracking';
 import './styles/main.scss';
 
 function App() {
+  usePageTracking();
   return (
-    <Router>
-      <React.Fragment>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/implementations">
-            <Implementations />
-          </Route>
-          <Route exact path="/gdpr">
-            <Gdpr />
-          </Route>
-          <Route exact path="/cookies">
-            <Cookies />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-      </React.Fragment>
-    </Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/implementations">
+          <Implementations />
+        </Route>
+        <Route exact path="/gdpr">
+          <Gdpr />
+        </Route>
+        <Route exact path="/cookies">
+          <Cookies />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
   );
 }
 
