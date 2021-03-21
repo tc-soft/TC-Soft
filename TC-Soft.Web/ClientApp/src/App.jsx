7458bb8983f0316755209c02ec078529df249f46
Tadeusz Ciszewski <office@tc-soft.pl>
2021-03-21 20:46:55 +0100

    feat: Add route to Instruction01

--- TC-Soft.Web/ClientApp/src/App.jsx
+++ TC-Soft.Web/ClientApp/src/App.jsx
@@ -8,6 +8,8 @@ import GdprPage from './pages/GdprPage';
 import CookiesPage from './pages/CookiesPage';
 import usePageTracking from './components/usePageTracking';
 
+import Instruction01 from './components/Instructions/Instructions/Instruction01/Instruction01';
+
 import './styles/main.scss';
 
 function App() {
@@ -22,6 +24,9 @@ function App() {
         </Route>
         <Route exact path="/instructions">
           <InstructionsPage />
+        </Route>
+        <Route exact path="/instructions/instruction01">
+          <Instruction01 />
         </Route>
         <Route exact path="/gdpr">
           <GdprPage />
