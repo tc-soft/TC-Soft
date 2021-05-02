63e3ecb9c252617e61add344c7e8e5eec1a84872
Tadeusz Ciszewski <office@tc-soft.pl>
2021-05-02 18:03:35 +0200

    feat: Add information about new instructions to InstructionInfo.jsx

--- TC-Soft.Web/ClientApp/src/components/Instructions/InstructionsInfo.jsx
+++ TC-Soft.Web/ClientApp/src/components/Instructions/InstructionsInfo.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Instruction1 from './Instruction1';
+// eslint-disable-next-line
 import Instruction2 from './Instruction2';
+// eslint-disable-next-line
 import Instruction3 from './Instruction3';
+// eslint-disable-next-line
 import Instruction4 from './Instruction4';
 
 function InstructionsInfo() {
@@ -16,6 +19,20 @@ function InstructionsInfo() {
                     name="Asystent w czasie nieobecności w systemie poczty Exchange"
                     description="Instrukcja opisująca ustawienie autorespondera poczty Exchange w programie Outlook"
                     link="/Instructions/Instruction01"
+                />
+                <Instruction1
+                    title="Poczta"
+                    date="18.04.2021 r."
+                    name="Konfiguracja konta Exchange na systemach Android"
+                    description="Instrukcja opisująca konfigurację poczty Exchange na telefonach z systemem Android"
+                    link="/Instructions/Instruction02"
+                />
+                <Instruction1
+                    title="Poczta"
+                    date="18.04.2021 r."
+                    name="Konfiguracja konta Exchange na systemach iOS"
+                    description="Instrukcja opisująca konfigurację poczty Exchange na telefonach z systemem iOS"
+                    link="/Instructions/Instruction03"
                 />
             </section>
         </React.Fragment>
